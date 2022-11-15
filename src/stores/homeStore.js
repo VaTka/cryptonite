31eb414c10c2d9c34395bba222365938a68de35b
VaTka <48445514+VaTka@users.noreply.github.com>
2022-11-15 16:06:19 +0200

    api work

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,8 +1,34 @@
 import create from 'zustand'
 import axios from "axios";
+import debounce from "../helpers/debounce";
 
 const homeStore = create((set) => ({
     coins: [],
+    trending: [],
+    query: '',
+
+    setQuery: (e) => {
+        set({query: e.target.value})
+        homeStore.getState().searchCoins()
+    },
+
+    searchCoins: debounce(async () => {
+        const {query, trending} = homeStore.getState();
+        if (query.length > 2) {
+            const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`);
+            const coins = res.data.coins.map(coin => {
+                return {
+                    name: coin.name,
+                    image: coin.large,
+                    id: coin.id
+                }
+            })
+            set({coins})
+        } else {
+            console.log(trending + "1212212122")
+            set({coins: trending})
+        }
+    }, 500),
 
     fetchCoins: async () => {
         const res = await axios.get(`https://api.coingecko.com/api/v3/search/trending`);
@@ -14,7 +40,8 @@ const homeStore = create((set) => ({
                 priceBtc: coin.item.priceBtc
             }
         })
-        set({coins})
+        console.log(coins)
+        set({coins, trending: coins})
     }
 }))
 
---
+++ src/stores/showStore.js
@@ -0,0 +1,27 @@
+import create from 'zustand'
+import axios from "axios";
+
+const showStore = create((set) => ({
+    graphData: [],
+
+    fetchData: async (id) => {
+        const [graphRes, dataRes] = await Promise.all([
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=200`),
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`),
+        ]);
+
+        const graphData = graphRes.data.prices.map((price) => {
+            const [timestamp, p] = price;
+            const date = new Date(timestamp).toLocaleDateString('en-GB')
+            return {
+                Date: date,
+                Price: p,
+            };
+        })
+        console.log(dataRes)
+        set({data: dataRes.data})
+        set({graphData})
+    },
+}));
+
+export default showStore
